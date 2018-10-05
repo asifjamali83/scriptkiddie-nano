ccf821d4ddaa1b345ac61d79878ccfc888b1c84f
noelxyz <koutan1113.2001@gmail.com>
2018-10-05 20:58:58 +0900

    Adapted free.py to PEP 8

--- free.py
+++ free.py
@@ -1,10 +1,15 @@
-# coding:utf-8
-from linepy import *
-import random, requests, shutil, codecs, json, random, os
+# coding: utf-8
+from linepy import LINE, OEPoll
+import random
+import requests
+import shutil
+import codecs
+import json
+import random
+import os
 yukino_1 = LINE()
 yukino_2 = LINE()
 yukino_3 = LINE()
-
 tracer = OEPoll(yukino_1)
 bots = [yukino_1, yukino_2, yukino_3]
 kickers = [yukino_2, yukino_3]
@@ -14,16 +19,14 @@ botMids = [yukino_1.getProfile().mid, yukino_2.getProfile().mid, yukino_3.getPro
 yukino_2Mid = yukino_2.getProfile().mid
 yukino_3Mid = yukino_3.getProfile().mid
 protect = {
-    'kickGroup' : {}, 
-    'normalProtect' : {},
-    'groupName' : {},
-    'groupPicture' : {},
-    'groupUrl' : {},
-    'blackUsers' : {},
-    'whiteUsers' : {}
+    'kickGroup': {}, 
+    'normalProtect': {},
+    'groupName': {},
+    'groupPicture': {},
+    'groupUrl': {},
+    'blackUsers': {},
+    'whiteUsers': {}
 }
-
-
 admins = ["",""]
 readData = codecs.open('blackUsers.json', 'r', 'utf-8')
 protect['blackUsers'] = json.load(readData)
@@ -36,16 +39,21 @@ helptext = "--このbotヘルプ--\n" \
             "/保護 オフ...保護機能をオフにします。"
 print(protect)
 print("success")
+
+
 def download_img(url, file_name):
     r = requests.get(url, stream=True)
     if r.status_code == 200:
         with open(file_name, 'wb') as f:
             r.raw.decode_content = True
             shutil.copyfileobj(r.raw, f)
+
+
 def random_string(length, seq='0123456789abcdefghijklmnopqrstuvwxyz'):
     sr = random.SystemRandom()
     return ''.join([sr.choice(seq) for i in range(length)])
-    
+
+
 def RECEIVE_MESSAGE(op):
     msg = op.message
     print(op)
@@ -67,11 +75,11 @@ def RECEIVE_MESSAGE(op):
                             yukino_1.sendMessage(msg.to, "保護機能はすでに有効になっています。")
                         else:
                             protect['normalProtect'][msg.to] = True
-                            readData = codecs.open('normalProtect.json','w','utf-8')
+                            readData = codecs.open('normalProtect.json', 'w', 'utf-8')
                             json.dump(protect['normalProtect'], readData, sort_keys=True, indent=4, ensure_ascii=False)
 
                             protect['kickGroup'][msg.to] = True
-                            readData = codecs.open('kickGroup.json','w','utf-8')
+                            readData = codecs.open('kickGroup.json', 'w', 'utf-8')
                             json.dump(protect['kickGroup'], readData, sort_keys=True, indent=4, ensure_ascii=False)
                             yukino_1.sendMessage(msg.to, "保護機能を有効にしました。")
                 elif msg.text == "/保護 オフ":
@@ -88,8 +96,12 @@ def RECEIVE_MESSAGE(op):
             except Exception as error:
                 print(error)
     except Exception as error:
-        print(error) 
+        print(error)
+
+
 tracer.addOpInterrupt(26, RECEIVE_MESSAGE)
+
+
 def NOTIFIED_KICKOUT_FROM_GROUP(op):
     print(op)
     try:
@@ -165,8 +177,8 @@ def NOTIFIED_KICKOUT_FROM_GROUP(op):
                     pass
                 else:
                     protect['blackUsers'][op.param2] = True
-                    f=codecs.open('blackUsers.json','w','utf-8')
-                    json.dump(protect['blackUsers'], f, sort_keys=True, indent=4,ensure_ascii=False)
+                    f=codecs.open('blackUsers.json', 'w', 'utf-8')
+                    json.dump(protect['blackUsers'], f, sort_keys=True, indent=4, ensure_ascii=False)
             elif op.param3 in yukino_1Mid:
                 K = random.choice(kickers)
                 K.kickoutFromGroup(op.param1, [op.param2])
@@ -190,7 +202,7 @@ def NOTIFIED_KICKOUT_FROM_GROUP(op):
                 else:
                     protect['blackUsers'][op.param2] = True
                     f=codecs.open('blackUsers.json','w','utf-8')
-                    json.dump(protect['blackUsers'], f, sort_keys=True, indent=4,ensure_ascii=False)
+                    json.dump(protect['blackUsers'], f, sort_keys=True, indent=4, ensure_ascii=False)
             else:
                 try:
                     yukino_3.kickoutFromGroup(op.param1, [op.param2])
@@ -205,10 +217,14 @@ def NOTIFIED_KICKOUT_FROM_GROUP(op):
                 else:
                     protect['blackUsers'][op.param2] = True
                     f=codecs.open('blackUsers.json','w','utf-8')
-                    json.dump(protect['blackUsers'], f, sort_keys=True, indent=4,ensure_ascii=False)
+                    json.dump(protect['blackUsers'], f, sort_keys=True, indent=4, ensure_ascii=False)
     except Exception as error:
         print(error)
+
+
 tracer.addOpInterrupt(19, NOTIFIED_KICKOUT_FROM_GROUP)
+
+
 def NOTIFIED_INVITE_INTO_GROUP(op):
     try:
         if profile.mid in op.param3:
@@ -237,7 +253,7 @@ def NOTIFIED_INVITE_INTO_GROUP(op):
                     G.preventedJoinByTicket = True
                     yukino_1.updateGroup(G)
         elif op.param1 in protect['normalProtect']:
-            Inviter = op.param3.replace("",',')
+            Inviter = op.param3.replace("", ',')
             InviterX = Inviter.split(",")
             matched_list = []
             for tag in protect['blackUsers']:
@@ -247,10 +263,14 @@ def NOTIFIED_INVITE_INTO_GROUP(op):
             else:
                 yukino_1.cancelGroupInvitation(op.param1, matched_list)
                 for i in bots:
-                    i.kickoutFromGroup(op.param1,[op.param2])
+                    i.kickoutFromGroup(op.param1, [op.param2])
     except Exception as error:
         print(error)
+
+
 tracer.addOpInterrupt(13, NOTIFIED_INVITE_INTO_GROUP)
+
+
 def NOTIFIED_UPDATE_GROUP(op):
     print(op)
     try:
@@ -287,10 +307,11 @@ def NOTIFIED_UPDATE_GROUP(op):
                         pass
                     else:
                         protect['blackUsers'][op.param2] = True
-                        f=codecs.open('blackUsers.json','w','utf-8')
-                        json.dump(protect['blackUsers'], f, sort_keys=True, indent=4,ensure_ascii=False)
+                        f=codecs.open('blackUsers.json', 'w', 'utf-8')
+                        json.dump(protect['blackUsers'], f, sort_keys=True, indent=4, ensure_ascii=False)
     except Exception as error:
         print(error)
+
 
 tracer.addOpInterrupt(11,NOTIFIED_UPDATE_GROUP)
 while True:
